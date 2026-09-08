e9c4e68acd124417cd9d951de438a7f1efa0fd57
Abdelhakim Akhadkhou <akhadkhou@gmail.com>
2026-09-08 11:10:38 +0300

    Initial commit: control using ultra sonic sensor

---
+++ wheel-control/wheel-control.ino
@@ -0,0 +1,70 @@
+const int TRIG_PIN = A0;
+const int ECHO_PIN = A1;
+
+const int ENA = 9;
+const int IN1 = 8;
+const int IN2 = 7;
+const int IN3 = 4;
+const int IN4 = 3;
+const int ENB = 2;
+
+const int TRIGGER_DISTANCE_CM = 20;
+const int MOTOR_SPEED = 200;
+
+void stopMotors() {
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+void driveForward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, MOTOR_SPEED);
+  analogWrite(ENB, MOTOR_SPEED);
+}
+
+long readDistanceCm() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duration = pulseIn(ECHO_PIN, HIGH, 30000);
+  if (duration == 0) {
+    return -1;
+  }
+  return duration * 0.034 / 2;
+}
+
+void setup() {
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
+  stopMotors();
+}
+
+void loop() {
+  long distance = readDistanceCm();
+
+  if (distance > 0 && distance < TRIGGER_DISTANCE_CM) {
+    driveForward();
+  } else {
+    stopMotors();
+  }
+
+  delay(60);
+}
